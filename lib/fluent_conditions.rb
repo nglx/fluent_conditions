861733d65d71f31dc594e330864985fa424c8469
pplcanfly <sleep.etc@gmail.com>
2011-09-12 22:01:07 +0200

    split method definitions into Instance/ClassMethods modules

--- lib/fluent_conditions.rb
+++ lib/fluent_conditions.rb
@@ -4,88 +4,89 @@ module FluentConditions
     base.instance_variable_set(:@builder, Class.new(Builder))
 
     base.class_eval do
-      def is
-        self.class.instance_variable_get(:@builder).new(self, :positive)
-      end
-
-      def is_not
-        self.class.instance_variable_get(:@builder).new(self, :negative)
-      end
+      include InstanceMethods
     end
 
-    base.instance_eval do
-      def fluent(field, options = {})
-        builder = instance_variable_get(:@builder)
+    base.extend ClassMethods
+  end
 
-        if options.include?(:values)
-          options[:values].each do |value|
-            builder.class_eval do
-              define_method(value) do
-                update_and_continue(instance_variable_get(:@object).send(field) == value)
-              end
+  module InstanceMethods
+    def is
+      self.class.instance_variable_get(:@builder).new(self, :positive)
+    end
 
-              define_method("#{value}?") do
-                update_and_finish(instance_variable_get(:@object).send(field) == value)
-              end
+    def is_not
+      self.class.instance_variable_get(:@builder).new(self, :negative)
+    end
+  end
 
-              define_method("not_#{value}") do
-                update_and_continue(instance_variable_get(:@object).send(field) != value)
-              end
+  module ClassMethods
+    def fluent(field, options = {})
+      builder = instance_variable_get(:@builder)
 
-              define_method("not_#{value}?") do
-                update_and_finish(instance_variable_get(:@object).send(field) != value)
-              end
-            end
-          end
-        elsif options.include?(:if)
-          field_name = options[:as]
-          condition_check = options[:if]
-
+      if options.include?(:values)
+        options[:values].each do |value|
           builder.class_eval do
-            define_method(field_name) do
-              update_and_continue(condition_check.call(instance_variable_get(:@object).send(field)))
+            define_method(value) do
+              update_and_continue(instance_variable_get(:@object).send(field) == value)
             end
 
-            define_method("#{field_name}?") do
-              update_and_finish(condition_check.call(instance_variable_get(:@object).send(field)))
-            end
-            
-            define_method("not_#{field_name}") do
-              update_and_continue(!condition_check.call(instance_variable_get(:@object).send(field)))
+            define_method("#{value}?") do
+              update_and_finish(instance_variable_get(:@object).send(field) == value)
             end
 
-            define_method("not_#{field_name}?") do
-              update_and_finish(!condition_check.call(instance_variable_get(:@object).send(field)))
-            end
-          end
-        else 
-          builder.class_eval do
-            define_method(field) do
-              update_and_continue(instance_variable_get(:@object).send(field))
+            define_method("not_#{value}") do
+              update_and_continue(instance_variable_get(:@object).send(field) != value)
             end
 
-            define_method("#{field}?") do
-              update_and_finish(instance_variable_get(:@object).send(field))
-            end
-
-            define_method("not_#{field}") do
-              update_and_continue(!instance_variable_get(:@object).send(field))
-            end
-
-            define_method("not_#{field}?") do
-              update_and_finish(!instance_variable_get(:@object).send(field))
+            define_method("not_#{value}?") do
+              update_and_finish(instance_variable_get(:@object).send(field) != value)
             end
           end
         end
-      end
+      elsif options.include?(:if)
+        field_name = options[:as]
+        condition_check = options[:if]
 
-      def fluent_values(field, values)
-        builder = instance_variable_get(:@builder)
+        builder.class_eval do
+          define_method(field_name) do
+            update_and_continue(condition_check.call(instance_variable_get(:@object).send(field)))
+          end
 
+          define_method("#{field_name}?") do
+            update_and_finish(condition_check.call(instance_variable_get(:@object).send(field)))
+          end
+
+          define_method("not_#{field_name}") do
+            update_and_continue(!condition_check.call(instance_variable_get(:@object).send(field)))
+          end
+
+          define_method("not_#{field_name}?") do
+            update_and_finish(!condition_check.call(instance_variable_get(:@object).send(field)))
+          end
+        end
+      else 
+        builder.class_eval do
+          define_method(field) do
+            update_and_continue(instance_variable_get(:@object).send(field))
+          end
+
+          define_method("#{field}?") do
+            update_and_finish(instance_variable_get(:@object).send(field))
+          end
+
+          define_method("not_#{field}") do
+            update_and_continue(!instance_variable_get(:@object).send(field))
+          end
+
+          define_method("not_#{field}?") do
+            update_and_finish(!instance_variable_get(:@object).send(field))
+          end
+        end
       end
     end
-
   end
+
 
   class Builder
     def initialize(object, type)
